86d89b835f8e848f624d7259cfa3057a8f764809
azizbarka <66383818+azizbarka@users.noreply.github.com>
2020-06-05 11:54:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Post'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('Users');
+mobileApp.tables.add('post'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('users');
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000););   // Listen for requests
     });
